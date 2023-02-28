6b4a446f09c7b036751681f864951d5cab8fc7bb
pol stam <polstam15@gmail.com>
2023-02-28 21:14:17 +0200

    firsttt

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const age = 40;
+
+
+switch (age) {
+    case 10:
+        console.log('you are ten');
+        break;
+    case 20:
+        console.log('your are 20 ');
+        break;
+    case 30:
+        break;
+    case 40:
+        console.log('you are 40');
+        break;
+    default:
+        console.log('not valid man');
+
+}
+
+
+var myname = 'pol';
+
+console.log(`${myname}`);
+
